f6c5da28afc03336b99ac98576fb5a93d1802751
TomJia98 <Tomchristian1998@gmail.com>
2022-03-22 19:55:58 +0800

    added helpers for banana (not working yet)

---
+++ utils/replaement-words.js
@@ -0,0 +1,26 @@
+const 
+
+const bananaWords = [
+  'banana',
+  'tree',
+  'Ook!',
+  'unga bunga',
+  '*mating call*',
+  'green banana',
+  'red banana',
+  'aaah!',
+  'Skree!',
+  'ooga ooga',
+  'woop!',
+  'bad sign language',
+  'gib me banana',
+  '-swings from tree-',
+  'human bad',
+  'Woo-ga Woot',
+  'ghibber',
+];
+
+function toBanana(string) {
+
+
+}
