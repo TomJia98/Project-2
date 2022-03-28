4efb74dbe377ed87528fb4462d8ebd3c9c96e415
TomJia98 <Tomchristian1998@gmail.com>
2022-03-28 12:17:02 +0800

    comments are now banana (!loggedIn)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,9 +5,9 @@ const { toBanana } = require('../utils/to-banana');
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await Post.findAll({
+    const posts1 = await Post.findAll({
       attributes: ['id', 'title', 'created_at', 'post_content'],
-      raw: true,
+
       include: [
         {
           model: Comment,
@@ -29,6 +29,9 @@ router.get('/', async (req, res) => {
         },
       ],
     });
+    const posts = posts1.map((post) => post.get({ plain: true }));
+    console.log(posts);
+
     posts.forEach(async (element) => {
       //getting all the reacts for each post (including in original post dupiplacates the posts based on reacts)
       const postsLikes = await React.findAll({
@@ -79,6 +82,80 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/post/api/posts/react', async (req, res) => {
+  //a copy of the like code for single post likes
+  const findReacts = await React.findAll({
+    raw: true,
+    where: {
+      post_id: req.body.id,
+      user_id: req.session.user_id,
+    },
+  });
+  if (
+    findReacts[0] === undefined &&
+    findReacts[0] === undefined
+    //first time user has liked content
+  ) {
+    console.log('no reacts -----------');
+    if (req.body.react) {
+      console.log('adding first like');
+      const firstLikeReact = await React.create({
+        user_id: req.session.user_id,
+        post_id: req.body.id,
+        like: true,
+        dislike: false,
+      });
+      firstLikeReact; //add their first like
+    } else if (!req.body.react) {
+      console.log('adding first dislike');
+      const firstDislikeReact = await React.create({
+        user_id: req.session.user_id,
+        post_id: req.body.id,
+        like: false,
+        dislike: true,
+      });
+      firstDislikeReact; //add their first dislike
+    }
+  } else if (findReacts[0].like) {
+    //if post has been liked already
+    console.log('already liked');
+    if (req.body.react === false) {
+      console.log('changing like to dislike');
+      //post has just been disliked
+      const changeLikeToDislike = await React.update(
+        {
+          like: false,
+          dislike: true,
+        },
+        {
+          where: {
+            id: findReacts[0].id,
+          },
+        }
+      );
+      changeLikeToDislike;
+    }
+  } else if (findReacts[0].dislike) {
+    console.log('already disliked');
+    if (req.body.react === true) {
+      console.log('changing dislike to like');
+      const changeDislikeToLike = await React.update(
+        {
+          like: true,
+          dislike: false,
+        },
+        {
+          where: {
+            id: findReacts[0].id,
+          },
+        }
+      );
+      changeDislikeToLike;
+    }
+  }
+  res.send('response');
+});
+
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
@@ -98,6 +175,7 @@ router.get('/signup', (req, res) => {
 });
 
 router.get('/post/:id', async (req, res) => {
+  //single homepage posts
   const post1 = await Post.findOne({
     where: {
       id: req.params.id,
@@ -140,6 +218,20 @@ router.get('/post/:id', async (req, res) => {
   post['likes'] = likeTally;
   post['dislikes'] = dislikeTally;
   console.log(post);
+  if (!req.session.loggedIn) {
+    const str = toBanana(post.title);
+    post.title = str[0];
+    const arr = toBanana(post.post_content);
+    post.post_content = arr[0];
+
+    post.comments.forEach((ele) => {
+      console.log(ele.comment_text);
+      if (ele.comment_text.length > 3) {
+        const newArr = toBanana(ele.comment_text);
+        ele.comment_text = newArr[0];
+      }
+    });
+  }
   res.render('single-post', {
     post,
     loggedIn: req.session.loggedIn,
